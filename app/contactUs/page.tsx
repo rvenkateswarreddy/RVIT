77f4162ea953c7d0729a47e745ca9a1000719225
rvenkateswarreddy <rvenkateswarreddy12345@gmail.com>
2025-07-31 18:29:46 +0530

    fix contactus

--- app/contactUs/page.tsx
+++ app/contactUs/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { FiMail, FiSend, FiPhone, FiMapPin } from 'react-icons/fi';
 import Image from 'next/image';
-import contact from '../../public/assets/project1.jpg';
+import contact from '../../public/assets/herosection3.png';
 import { db } from '../../FirebaseConfig';
 import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
 
